72a8d3cfc54b52dc02dbe07c2b9a658f496d532e
scadler <scadler@s.sfusd.edu>
2020-02-18 03:07:19 +0000

    ball moves and interacts with user paddle

--- script.js
+++ script.js
@@ -15,9 +15,9 @@ function drawCircle(x, y, r, color){
     context.fill();
 }
 //14:40
-function drawText(text, x, y, color){
+function drawText(text,x, y, color){
     context.fillStyle = color;
-    // context.font = "75px fantasy";
+    context.font = "75px fantasy";
     context.fillText(text, x, y);
 }
 function drawNet(){
@@ -28,6 +28,23 @@ function drawNet(){
 function collision(ball, paddle){
     paddle.top = paddle.y;
     paddle.bottom = paddle.y + paddle.height;
+    paddle.left = paddle.x;
+    paddle.right = paddle.x + paddle.width;
+    ball.top = ball.y - ball.radius;
+    ball.bottom = ball.y + ball.radius;
+    ball.left = ball.x - ball.radius;
+    ball.right = ball.x + ball.radius;
+    return ball.right > paddle.left && ball.top < paddle.bottom && ball.left < paddle.right && ball.bottom > paddle.top;
+}
+function resetBall(){
+    ball.x = canvas.width/2;
+    ball.y = canvas.height/2;
+    ball.speed = 5;
+    ball.velocityX = -ball.velocityX
+}
+function movePaddle(event){
+    let rect = canvas.getBoundingClientRect();
+    user.y = event.clientY - rect.top - user.height/2;
 }
 //defining constants
 const user = {
@@ -65,24 +82,51 @@ const ball = {
 const framesPerSecond = 50;
 //calling functions
 function update(){
-    ball.x = ball.velocityX;
+    ball.x += ball.velocityX;
     ball.y += ball.velocityY;
     if( ball.y + ball.radius > canvas.height || ball.y - ball.radius < 0){
         //ball has hit top or bottom
-        velocityY = - velocityY;
+        ball.velocityY = - ball.velocityY;
+    }
+    // this is saying if (ball.x < canvas.width/2 (on the right side)){ paddle = user} else {paddle = comp}
+    //30:46
+    let paddle = (ball.x < canvas.width/2) ? user : comp;
+    if( collision(ball,paddle) ){
+        //need to find point of impact so rebound angle can be calculated
+        let collidePoint = (ball.y - (paddle.y + paddle.height/2));
+        collidePoint = collidePoint / (paddle.height/2);
+        let angleRadian = (Math.PI/4) * collidePoint;
+        //the ball moves right after hitting user paddle, left after comp
+        let direction = (ball.x < canvas.width/2) ? 1 : -1;
+        ball.velocityX = direction * ball.speed * Math.cos(angleRadian)
+        ball.velocityY = direction * ball.speed * Math.sin(angleRadian)
+        //the ball moves faster every rebound
+        ball.speed += 0.1;
+    }
+    if(ball.x - ball.radius < 0){
+        comp.score++;
+        resetBall();
+    }else if(ball.x + ball.radius > canvas.width){
+        user.score++;
+        resetBall();
     }
 }
 function render(){
 drawRect(0, 0, canvas.width, canvas.height, "black");
-drawText(user.score, canvas.width/4, canvas.hieght/5,  "White");
-drawText(comp.score, 3*canvas.width/4, canvas.hieght/5,  "White");
+drawText(0, 40, 60,  "White");
+drawText(user.score, canvas.width/4, canvas.hieght/5, "White");
+drawText(comp.score, 3*canvas.width/4, canvas.hieght/5, "White");
 drawNet();
+//user & comp paddles
 drawRect(user.x, user.y, user.width, user.height, user.color);
 drawRect(comp.x, comp.y, comp.width, comp.height, comp.color);
+//ball
 drawCircle(ball.x, ball.y, ball.radius, ball.color)
 }
 function game(){
-    update();
     render();
+    update();
 }
-setInterval(game(), 1000/framesPerSecond);
+setInterval(game, 1000/framesPerSecond);
+
+canvas.addEventListener("mousemove", movePaddle);
